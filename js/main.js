5e03f4f5b77e18e04db124b69501bf4ac2e93e40
smartboy0310 <qdustudent6@gmail.com>
2021-09-08 23:38:36 +0500

    JavaScript Arithmetic operators

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ var userHotelExpenses = 250*9433.34;
 var userMuseumExpenses = 120*10354.03;
 
 if (userMoney >= (userRoadExpenses + userHotelExpenses + userMuseumExpenses)) {
-   console.log(`“Oq yo’l, ${userName}!”`)
+   console.log(`“Oq yo’l, ${userName}!”`);
+   alert(`“Oq yo’l, ${userName}!”`);
 }
 else {
-   console.log(`“${userName}, ozgina sabr qilish kerak bo’lar ekansiz.”`)
+   console.log(`“${userName}, ozgina sabr qilishingiz kerak bo’lar ekan.”`);
+   alert(`“${userName}, ozgina sabr qilishingiz kerak bo’lar ekan.”`);
 }
